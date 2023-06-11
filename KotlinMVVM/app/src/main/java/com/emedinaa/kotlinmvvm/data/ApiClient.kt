1ef89c9eafda2e5a77caeb32ab1ffd9b70603ae9
Eduardo José Medina Alfaro <emedinaa@gmail.com>
2023-06-10 21:49:17 -0500

    upgrade okhttp library and base url

--- KotlinMVVM/app/src/main/java/com/emedinaa/kotlinmvvm/data/ApiClient.kt
+++ KotlinMVVM/app/src/main/java/com/emedinaa/kotlinmvvm/data/ApiClient.kt
@@ -12,31 +12,31 @@ import retrofit2.http.GET
  */
 object ApiClient {
 
-    //https://obscure-earth-55790.herokuapp.com/api/museums
-    private const val API_BASE_URL = "https://obscure-earth-55790.herokuapp.com"
+    //https://emedinaa.azurewebsites.net/api/museums
+    private const val API_BASE_URL = "https://emedinaa.azurewebsites.net"
 
     private var servicesApiInterface: ServicesApiInterface? = null
-
-    fun build(): ServicesApiInterface? {
-        var builder: Retrofit.Builder = Retrofit.Builder()
-            .baseUrl(API_BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-
-        var httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
-        httpClient.addInterceptor(interceptor())
-
-        var retrofit: Retrofit = builder.client(httpClient.build()).build()
-        servicesApiInterface = retrofit.create(
-            ServicesApiInterface::class.java
-        )
-
-        return servicesApiInterface as ServicesApiInterface
-    }
 
     private fun interceptor(): HttpLoggingInterceptor {
         val httpLoggingInterceptor = HttpLoggingInterceptor()
         httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         return httpLoggingInterceptor
+    }
+
+    fun build(): ServicesApiInterface? {
+        val builder: Retrofit.Builder = Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+
+        val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
+        httpClient.addInterceptor(interceptor())
+
+        val retrofit: Retrofit = builder.client(httpClient.build()).build()
+        servicesApiInterface = retrofit.create(
+            ServicesApiInterface::class.java
+        )
+
+        return servicesApiInterface as ServicesApiInterface
     }
 
     interface ServicesApiInterface {
